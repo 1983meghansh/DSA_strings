0544e951bfe61eab68cd0015fafa55215149ebb5
1983meghansh <mundrameghansh1983@gmail.com>
2021-08-22 20:07:24 +0530

    str2

---
+++ allsubsequences.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+void subs(string input,string output)
+{
+    if(input.length()==0)
+    {
+        cout<<output<<endl;
+        return;
+    }
+    subs(input.substr(1),output);
+    subs(input.substr(1),output+input[0]);
+}
+int main()
+{
+    string input;
+    cin>>input;
+    string output="";
+    subs(input,output);
+}
